81b74557ab8866ae4bb1e7c939360f186541dad8
Thuan Nguyen <thuannguyenk17gtvt@gmail.com>
2021-04-15 10:14:37 +0700

    tạo component favoriteproduct

---
+++ src/app/client/profile/fovoriteproduct/fovoriteproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fovoriteproduct',
+  templateUrl: './fovoriteproduct.component.html',
+  styleUrls: ['./fovoriteproduct.component.css']
+})
+export class FovoriteproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
