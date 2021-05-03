250b7d1ddf4f3aff790a4007d06128aa4cc41c05
Tuấn <ntuan9870@gmail.com>
2021-05-03 21:22:28 +0700

    Sửa lỗi tìm kiếm sản phẩm nhập và làm chi tiết tồn kho

---
+++ src/app/models/detail-whproduct.model.spec.ts
@@ -0,0 +1,7 @@
+import { DetailWHProduct } from './detail-whproduct.model';
+
+describe('DetailWHProduct', () => {
+  it('should create an instance', () => {
+    expect(new DetailWHProduct()).toBeTruthy();
+  });
+});
